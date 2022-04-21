31ee42ff3228f8a0367243b4682519b44e254ee3
Alberto <info@albertoperipolli.com>
2022-04-21 15:24:05 +0200

    Remove entities type null

--- src/Resources/Entities.php
+++ src/Resources/Entities.php
@@ -4,7 +4,6 @@ namespace Datomatic\FattureInCloud\Resources;
 
 abstract class Entities extends Resource
 {
-    public const TYPE_NULL = 'null';
     public const TYPE_COMPANY = 'company';
     public const TYPE_PERSON = 'person';
     public const TYPE_PA = 'pa';
