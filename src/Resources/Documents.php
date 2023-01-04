521f6518ed9ab164ea21e5523e3b164af5a0ecee
trippo <trippo@users.noreply.github.com>
2023-01-04 14:57:57 +0000

    Fix styling

--- src/Resources/Documents.php
+++ src/Resources/Documents.php
@@ -7,12 +7,14 @@ use Datomatic\FattureInCloud\Enums\DocumentType;
 abstract class Documents extends Resource
 {
     protected DocumentType $type;
+
     public function all(array $params = []): array
     {
         $params['type'] = $this->type->value;
 
         return parent::all($params);
     }
+
     public static function availableLanguages(): array
     {
         return ['it', 'en', 'de', 'fr', 'es', 'el', 'pl', 'sl'];
