19ae480c48b2e91fe50324f113fbc5a71a39031d
Alberto Peripolli <info@albertoperipolli.com>
2023-01-04 15:27:08 +0100

    fix type in all and getById methods

--- src/Resources/Documents.php
+++ src/Resources/Documents.php
@@ -7,14 +7,12 @@ use Datomatic\FattureInCloud\Enums\DocumentType;
 abstract class Documents extends Resource
 {
     protected DocumentType $type;
-
     public function all(array $params = []): array
     {
-        $params['type'] = $this->type;
+        $params['type'] = $this->type->value;
 
         return parent::all($params);
     }
-
     public static function availableLanguages(): array
     {
         return ['it', 'en', 'de', 'fr', 'es', 'el', 'pl', 'sl'];
